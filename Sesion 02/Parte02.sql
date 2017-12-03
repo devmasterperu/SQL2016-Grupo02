5e398112a5c979867bcdbecdbdbfa428d853356b
devmasterperu <32893765+devmasterperu@users.noreply.github.com>
2017-12-03 18:54:29 -0500

    Update Parte02.sql

--- Sesion 02/Parte02.sql
+++ Sesion 02/Parte02.sql
@@ -1,45 +1,44 @@
-
 --Taller de Ordenamiento y Filtrado
---1.Toda vez que se requiera listar los paÌses, la base de datos debe enviar el id,
---nombre de cada paÌs ordenado por nombre de manera ascendente
---1.1 Generar sentencias en Excel para inserciÛn.
---1.2 Ejecutar inserciÛn sobre tabla tbPais
+--1.Toda vez que se requiera listar los pa√≠ses, la base de datos debe enviar el id,
+--nombre de cada pa√≠s ordenado por nombre de manera ascendente
+--1.1 Generar sentencias en Excel para inserci√≥n.
+--1.2 Ejecutar inserci√≥n sobre tabla tbPais
 --1.3 Elaborar consulta con lo solicitado.
 select   idpais,
          nombrepais
 from     tbPais 
 order by nombrepais asc
 
---2.Elaborar un reporte que liste las pelÌculas registradas entre el 26/11/2017 y el 01/12/2017
---Adem·s,si es una pelÌcula peruana debe mostrar el mensaje ëPelÌcula Nacionalí. 
---Caso contrario, ëPelÌcula del extranjeroí
---2.1 InserciÛn de pelÌcula extranjera.
+--2.Elaborar un reporte que liste las pel√≠culas registradas entre el 26/11/2017 y el 01/12/2017
+--Adem√°s,si es una pel√≠cula peruana debe mostrar el mensaje ‚ÄòPel√≠cula Nacional‚Äô. 
+--Caso contrario, ‚ÄòPel√≠cula del extranjero‚Äô
+--2.1 Inserci√≥n de pel√≠cula extranjera.
 insert into tbPelicula
 select 2,'JASON X','...','http://devmaster.pe/devflix/jasonx',1990,90,getdate(),null
---2.2 An·lisis de formato de fecha: 2017-11-26 13:07:03.303->20171126 (Formato 112)
+--2.2 An√°lisis de formato de fecha: 2017-11-26 13:07:03.303->20171126 (Formato 112)
 --2.3 Elaborar consulta con lo solicitado. 
 select nombre,
        convert(varchar(8),fecregistro,112) as fecregistro,
-       case when idpais=1 then 'PelÌcula Nacional' else 'PelÌcula del extranjero' end as Mensaje
+       case when idpais=1 then 'Pel√≠cula Nacional' else 'Pel√≠cula del extranjero' end as Mensaje
 from   tbPelicula
 where  convert(varchar(8),fecregistro,112)>='20171126' and convert(varchar(8),fecregistro,112)<='20171201'
 
---3.Elaborar un reporte que liste las pelÌculas cuyo nombre contenga la palabra ëMAí. 
---Adem·s, considere el siguiente cuadro para mostrar en base a la duraciÛn en minutos un mensaje
+--3.Elaborar un reporte que liste las pel√≠culas cuyo nombre contenga la palabra ‚ÄòMA‚Äô. 
+--Adem√°s, considere el siguiente cuadro para mostrar en base a la duraci√≥n en minutos un mensaje
 select nombre,
        duracionmin,
-case   when duracionmin>=0 and duracionmin<=59 then 'PelÌcula no cumple los est·ndares de la plataforma'
-       when duracionmin>=60 and duracionmin<=90 then 'PelÌcula '+nombre+' es de duraciÛn baja'
-       when duracionmin>=91 and duracionmin<=120 then 'PelÌcula '+nombre+' es de duraciÛn media'
-       else 'PelÌcula '+nombre+' es de duraciÛn alta' end as mensaje_pelicula
+case   when duracionmin>=0 and duracionmin<=59 then 'Pel√≠cula no cumple los est√°ndares de la plataforma'
+       when duracionmin>=60 and duracionmin<=90 then 'Pel√≠cula '+nombre+' es de duraci√≥n baja'
+       when duracionmin>=91 and duracionmin<=120 then 'Pel√≠cula '+nombre+' es de duraci√≥n media'
+       else 'Pel√≠cula '+nombre+' es de duraci√≥n alta' end as mensaje_pelicula
 from   tbPelicula
-where  nombre like '%MA%'--Obtener las pelÌculas con MA dentro del nombre
+where  nombre like '%MA%'--Obtener las pel√≠culas con MA dentro del nombre
 
 --4.Elaborar un reporte que liste en una sola consulta
---4.1 PelÌculas con aÒo de estreno 2015 y duraciÛn en minutos mayor a 100
---4.2 PelÌculas con aÒo de estreno 2016 hacia adelante.
---Usando Operadores LÛgicos
---Tabla de verdad: PvQ.Si al menos una de las expresiones es verdadera. la fila ser· devueltas en SQL. 
+--4.1 Pel√≠culas con a√±o de estreno 2015 y duraci√≥n en minutos mayor a 100
+--4.2 Pel√≠culas con a√±o de estreno 2016 hacia adelante.
+--Usando Operadores L√≥gicos
+--Tabla de verdad: PvQ.Si al menos una de las expresiones es verdadera. la fila ser√° devueltas en SQL. 
 select nombre,estreno,duracionmin from tbPelicula
 where (estreno=2015 and duracionmin>100) --P
 			OR 
@@ -52,7 +51,7 @@ union all
 select nombre,estreno,duracionmin from tbPelicula
 where estreno>=2016
 
---5.Elaborar un reporte que liste las pelÌculas no estrenadas en el aÒo 2015
+--5.Elaborar un reporte que liste las pel√≠culas no estrenadas en el a√±o 2015
 --Forma 1
 select nombre,estreno,duracionmin from tbPelicula
 where NOT (estreno=2015)
@@ -67,24 +66,24 @@ select nombre,estreno,duracionmin from tbPelicula
 where estreno NOT IN (2015)
 
 --6.En base al requerimiento 1, elabore un reporte que liste los 10 primeros
---paÌses ordenados por nombre de manera descendente. Este reporte recibe un
---par·metro que indica si incluir· filas con valores que coinciden con la
---posiciÛn 10
+--pa√≠ses ordenados por nombre de manera descendente. Este reporte recibe un
+--par√°metro que indica si incluir√° filas con valores que coinciden con la
+--posici√≥n 10
 
---Variable que controla que reporte se mostrar·
+--Variable que controla que reporte se mostrar√°
 declare @siempates bit
 set     @siempates=1
 
---LÛgica de reporte 
+--L√≥gica de reporte 
 if @siempates=0
 begin
-	--Se muestran sÛlo 10 registros y UGANDA se muestra una sola vez
+	--Se muestran s√≥lo 10 registros y UGANDA se muestra una sola vez
 	select top 10 nombrepais,idpais from tbPais
 	order by nombrepais desc
 end
 else
 begin
-	--Se muestran 11 registros y UGANDA se muestra m·s de una vez
+	--Se muestran 11 registros y UGANDA se muestra m√°s de una vez
 	select top 10 with ties nombrepais,idpais from tbPais
 	order by nombrepais desc
 end
@@ -103,13 +102,13 @@ where
  --Uso de BETWEEN
  select nombre,
        convert(varchar(8),fecregistro,112) as fecregistro,
-       case when idpais=1 then 'PelÌcula Nacional' else 'PelÌcula del extranjero' end as Mensaje
+       case when idpais=1 then 'Pel√≠cula Nacional' else 'Pel√≠cula del extranjero' end as Mensaje
 from   tbPelicula
 where  convert(varchar(8),fecregistro,112) BETWEEN '20171126' and '20171203'
 --Usando IN en vez de BETWEEN
  select nombre,
        convert(varchar(8),fecregistro,112) as fecregistro,
-       case when idpais=1 then 'PelÌcula Nacional' else 'PelÌcula del extranjero' end as Mensaje
+       case when idpais=1 then 'Pel√≠cula Nacional' else 'Pel√≠cula del extranjero' end as Mensaje
 from   tbPelicula
 where  convert(varchar(8),fecregistro,112) in ('20171126','20171127','20171128','20171129','20171130',
 '20171201','20171202','20171203')
